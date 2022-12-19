d040be993defad63953cdbfbacda70f61e1c402d
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2022-12-19 22:37:00 +0100

    Initial commit

---
+++ Intel8086/Intel8086/Memory.cs
@@ -0,0 +1,89 @@
+﻿using System.Globalization;
+
+namespace Intel8086;
+
+public class Memory
+{
+    private short AX, BX, CX, DX = 0;
+    
+    public Memory()
+    {
+        AX = BX = CX = DX = 0;
+    }
+
+    public bool MOV(string addressFROM, string addressTO)
+    {
+        ref short registry = ref AX;
+        if (addressFROM.ToLower().Contains("ax"))
+        {
+            registry = ref AX;
+        }else if (addressFROM.ToLower().Contains("bx"))
+        {
+            registry = ref BX;
+        }else if (addressFROM.ToLower().Contains("cx"))
+        {
+            registry = ref CX;
+        }else if (addressFROM.ToLower().Contains("dx"))
+        {
+            registry = ref DX;
+        }
+        else
+        {
+            return false;
+        }
+        
+        if (addressTO.ToLower().Contains("ax"))
+        {
+            registry = AX;
+        }else if (addressTO.ToLower().Contains("bx"))
+        {
+            registry = BX;
+        }else if (addressTO.ToLower().Contains("cx"))
+        {
+            registry = CX;
+        }else if (addressTO.ToLower().Contains("dx"))
+        {
+            registry = DX;
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool ADD(string address, string value)
+    {
+        short val;
+        val = short.Parse(value);
+        if (address.ToLower().Contains("ax"))
+        {
+            AX = val;
+        }
+        else if (address.ToLower().Contains("bx"))
+        {
+            BX = val;
+        }
+        else if (address.ToLower().Contains("cx"))
+        {
+            CX = val;
+        }
+        else if (address.ToLower().Contains("dx"))
+        {
+            DX = val;
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public short AND()
+    {
+        AX &= BX;
+        return AX;
+    }
+}
---
+++ Intel8086/Intel8086/Program.cs
@@ -0,0 +1,36 @@
+﻿using Intel8086;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Memory memory = new Memory();
+        while (true)
+        {
+            Console.WriteLine("Press RETURN");
+            Console.In.ReadLine();
+            Console.Clear();
+            Console.Out.WriteLine("Command list:\n\tMOV [AX,BX,CX,DX] [AX,BX,CX,DX]\n\tADD [AX,BX,CX,DX] [value]\n\tAND\n\tQUIT");
+            string input = Console.In.ReadLine();
+            string[] commands = input.Split(' ');
+            if (commands[0].ToLower().Equals("mov"))
+            {
+                if (memory.MOV(commands[1], commands[2]))
+                    continue;
+            }else if (commands[0].ToLower().Equals("add"))
+            {
+                if (memory.ADD(commands[1], commands[2]))
+                    continue;
+            }else if (commands[0].ToLower().Equals("and"))
+            {
+                Console.Out.WriteLine("Result: " + memory.AND());
+                continue;
+            }else if (commands[0].ToLower().Contains("quit"))
+                break;
+            Console.Out.WriteLine("Wrong input, press ENTER to continue");
+        }
+
+    }
+
+}
